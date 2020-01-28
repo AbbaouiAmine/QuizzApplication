e42b5e0185cfb24bdf259e53f39330525c30d3ba
Amine Abbaoui <abbaouiamine.r@gmail.com>
2020-01-28 11:04:15 +0100

    first commit

---
+++ app/src/main/java/com/amineabbaoui/quizapp_o2/GPSActivity.java
@@ -0,0 +1,14 @@
+package com.amineabbaoui.quizapp_o2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GPSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps);
+    }
+}
